7db642a85c2c148f4d38e63a85d4e5cbb718fef0
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-12-31 12:02:13 -0600

    created new Component for movie cards

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,11 +1,9 @@
 import React, {useState} from "react";
-import './searchMovies.css'
+import './CSS/searchMovies.css'
+import MovieCard from './MovieCard'
 
 export default function SearchMovies(){
-    
-    //states- input query, movies
     const [query, setQuery] = useState('');
-    //create the state for movies, and update that state appropriate
     const [movies, setMovies] = useState([]);
     
     const searchMovies = async (e) => {
@@ -34,22 +32,9 @@ export default function SearchMovies(){
                 <button className="button" type="submit">Search</button>
             </form>
             <div className="card-list">
-                {movies.filter(movie => movie.poster_path).map(movie => (
-                  
-                    <div className="card" key={movie.id}>
-                        <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                            />
-                        <div className="card--content">
-                        <h3 className="card--title">{movie.title}</h3>
-                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                        <p><small>RATING: {movie.vote_average}</small></p>
-                        <p className="card--desc">{movie.overview}</p>
-                        </div>
-
-                    </div>
-                ))}
+              {movies.filter(movie => movie.poster_path).map(movie => (
+                <MovieCard movie={movie}/>
+              ))}
             </div>    
         </>
     )
