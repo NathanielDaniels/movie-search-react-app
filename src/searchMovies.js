ae23dd41621de1a978a355b6576f0261c01dcd96
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-03 12:16:18 -0600

    blank input error warning now working

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -8,20 +8,23 @@ export default function SearchMovies() {
 
   const searchMovies = async (e) => {
     e.preventDefault()
-
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
-
-    //? Filter Button
-    const btn = document.createElement('button')
-    btn.classList.add('filter-button');
-    btn.setAttribute('type', 'button')
-    btn.innerHTML = `Filter by rating`;
-    document.querySelector('.card-list').appendChild(btn)
-
-    //? Movie Search
     const formInput = document.querySelector('form > .input')
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+    
     if (formInput.value !== '') {
       formInput.placeholder = "i.e. Jurassic Park"
+
+      //? Remove Red Placeholder Text if search successful. 
+      formInput.classList.value = 'input'
+
+       //? Add Filter Button
+       //! Filter Button only works on first search (need to find bug)
+      const btn = document.createElement('button')
+      btn.classList.add('filter-button');
+      btn.setAttribute('type', 'button')
+      btn.innerHTML = `Filter by rating`;
+      document.querySelector('.card-list').appendChild(btn)
+
       try {
         //? Fetch Data from movie API
         const response = await fetch(url)
@@ -40,12 +43,12 @@ export default function SearchMovies() {
         console.error(err)
       }
     } else {
+      formInput.classList.add('input-placeholder')
       formInput.placeholder = "Movie Title Required!"
     }
 
     //? reset Input Value
     setQuery('')
-    
   }
   return (
     <>
