c59c16879415bf87798193b0318f5c9be230f47d
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2021-01-03 08:03:07 -0600

    removing old commented code | MovieCard

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -11,17 +11,18 @@ export default function SearchMovies() {
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
+    //? Filter Button
+    const btn = document.createElement('button')
+    btn.classList.add('filter-button');
+    btn.setAttribute('type', 'button')
+    btn.innerHTML = `Filter by rating`;
+    document.querySelector('.card-list').appendChild(btn)
+
     try {
+      //? Fetch Data from movie API
       const response = await fetch(url)
       const data = await response.json()
       setMovies(data.results)
-
-      //? Filter Button
-      const btn = document.createElement('button')
-      btn.classList.add('filter-button');
-      btn.setAttribute('type', 'button')
-      btn.innerHTML = `Filter by rating`;
-      document.querySelector('.card-list').appendChild(btn)
 
       //? filter data by rating
       document.querySelector('.filter-button').onclick = function() {
